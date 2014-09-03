8b16c036180ad0122e8540c7cb66fe3596c2e817
Ian Ward <ian@excess.org>
2014-09-03 10:51:21 -0400

    fluent setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import sys, os
 version = '0.0.1'
 
 setup(
-    name='ckanext-scheming',
+    name='ckanext-fluent',
     version=version,
-    description="Easy, sharable custom CKAN schemas",
+    description="Multilingual fields for CKAN",
     long_description="""
     """,
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -16,7 +16,7 @@ setup(
     url='',
     license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext', 'ckanext.scheming'],
+    namespace_packages=['ckanext', 'ckanext.fluent'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -25,11 +25,6 @@ setup(
     entry_points=\
     """
     [ckan.plugins]
-    scheming_datasets=ckanext.scheming.plugins:SchemingDatasetsPlugin
-    scheming_groups=ckanext.scheming.plugins:SchemingGroupsPlugin
-    scheming_organizations=ckanext.scheming.plugins:SchemingOrganizationsPlugin
-
-    [paste.paster_command]
-    scheming=ckanext.scheming.commands:SchemingCommand
+    fluent=ckanext.fluent.plugins.FluentPlugin
     """,
 )

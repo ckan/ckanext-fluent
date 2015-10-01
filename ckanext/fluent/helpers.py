27038a03fde85e9e7e7f35fa7cdefee1b5f08fd8
Ian Ward <ian@excess.org>
2015-10-01 09:46:48 -0400

    fluent_form_label feature

--- ckanext/fluent/helpers.py
+++ ckanext/fluent/helpers.py
@@ -1,5 +1,7 @@
-
 from ckan.lib.helpers import get_available_locales
+
+from ckanext.scheming.helpers import scheming_language_text
+
 
 def fluent_form_languages(schema, field=None):
     """
@@ -19,3 +21,19 @@ def fluent_form_languages(schema, field=None):
         if l.language not in langs:
             langs.append(l.language)
     return langs
+
+def fluent_form_label(field, lang):
+    """
+    Return a label for the input field for the given language
+
+    If the field has a fluent_form_label defined the label will
+    be taken from there.  If a matching label can't be found
+    this helper will return the language code in uppercase and
+    the standard label.
+    """
+    form_label = field.get('fluent_form_label', {})
+
+    if lang in form_label:
+        return scheming_language_text(form_label[lang])
+
+    return lang.upper() + ' ' + scheming_language_text(field['label'])

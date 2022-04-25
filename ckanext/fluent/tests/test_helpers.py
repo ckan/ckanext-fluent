e8a3c17ab74666866a6ba382e2a8c6b061661404
Andres Vazquez <andres@data99.com.ar>
2022-04-25 11:30:35 -0300

    Update tests

--- ckanext/fluent/tests/test_helpers.py
+++ ckanext/fluent/tests/test_helpers.py
@@ -6,20 +6,56 @@ from ckanext.fluent.helpers import (
 
 
 class TestFluentHelpers(object):
-    """ TODO """
 
-    def test_fluent_form_languages(self):
-        """ Not a real test. """
-        res = fluent_form_languages()
-
-    def test_fluent_alternate_languages(self):
-        """ Not a real test. """
-        res = fluent_alternate_languages()
-
-    def test_fluent_form_label(self):
-        """ Not a real test. """
+    def test_fluent_form_languages_field(self):
         field = {
-            'label': 'some label'
+            'form_languages': ['en', 'es']
+        }
+        res = fluent_form_languages(field=field)
+        assert res == ['en', 'es']
+
+    def test_fluent_form_languages_schema(self):
+        schema = {
+            'form_languages': ['en', 'fr']
+        }
+        res = fluent_form_languages(schema=schema)
+        assert res == ['en', 'fr']
+
+    def test_fluent_alternate_languages_field(self):
+        field = {
+            'alternate_languages': {'en': ['en-GB']}
+        }
+        res = fluent_alternate_languages(field=field)
+        assert res == {'en': ['en-GB']}
+
+    def test_fluent_alternate_languages_schema(self):
+        schema = {
+            'alternate_languages': {'en': ['en-GB']}
+        }
+        res = fluent_alternate_languages(schema=schema)
+        assert res == {'en': ['en-GB']}
+
+    def test_fluent_form_label_exists(self):
+        
+        field = {
+            'fluent_form_label': {
+                'en': 'English label',
+                'fr': 'French label'
+            }
         }
         lang = 'en'
         res = fluent_form_label(field, lang)
+        assert res == 'English label'
+
+    def test_fluent_form_label_not_exists(self):
+        
+        field = {
+            'fluent_form_label': {
+                'en': 'English label',
+                'fr': 'French label'
+            },
+            'label': 'Standard label'
+        }
+        lang = 'es'
+        res = fluent_form_label(field, lang)
+        assert res == 'ES Standard label'
